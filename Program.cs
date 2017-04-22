9886382baf0875bc346a69de89ef8e1162e97a70
Rejuvination <westy9000@hotmail.com>
2017-04-22 19:13:21 -0400

    It should Kinda work I hope...

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            string TheirName;
+            string Correct = "BREAK";
+            Console.WriteLine("What is your name?");
+            TheirName = Console.ReadLine();
+            Console.WriteLine("Hello " + TheirName);
+            
+            Console.ReadKey();
+            if (TheirName != Correct) { goto Start; }
+            Console.WriteLine(TheirName);
+        }
+        
+    }
+}
